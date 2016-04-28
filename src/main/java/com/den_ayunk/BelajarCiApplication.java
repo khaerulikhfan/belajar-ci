33073a8c5ba6a73c9899f9b34e61bd08b0619e89
waroengponcol <repositori@waroengponcol.xyz>
2016-04-28 18:14:40 +0700

    commit suskes harusnya pada travis

--- src/main/java/com/den_ayunk/BelajarCiApplication.java
+++ src/main/java/com/den_ayunk/BelajarCiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BelajarCiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BelajarCiApplication.class, args);
+		//SpringApplication.run(BelajarCiApplication.class, args);
 	}
 }
