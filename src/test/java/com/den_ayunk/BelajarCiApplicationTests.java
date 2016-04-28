70738ba25c6ab58e036f95455a61bd2b73399d36
waroengponcol <repositori@waroengponcol.xyz>
2016-04-28 13:45:29 +0700

    commit pertama, isinya project yang dibuat dengan spring.io

---
+++ src/test/java/com/den_ayunk/BelajarCiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.den_ayunk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BelajarCiApplication.class)
+@WebAppConfiguration
+public class BelajarCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
