9146833a2a15e3d25348ec0e14d17d183eca02ef
waroengponcol <repositori@waroengponcol.xyz>
2016-04-28 18:11:53 +0700

    commit ini harusnya ketika dibuild pada travis sukses, karena databasenya udah dibenerin

--- src/main/java/com/den_ayunk/BelajarCiApplication.java
+++ src/main/java/com/den_ayunk/BelajarCiApplication.java
@@ -8,6 +8,5 @@ public class BelajarCiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BelajarCiApplication.class, args);
-                System.out.print("hai");
 	}
 }
